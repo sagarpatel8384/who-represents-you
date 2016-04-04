e3c74598c45f97ef1aa3f34978dbbafc05a268cc
shirleylberry <shirleylberry@gmail.com>
2016-04-04 00:19:16 -0400

    adapter now works for house members

--- app/adapters/electedOfficialsAdapter.js
+++ app/adapters/electedOfficialsAdapter.js
@@ -5,17 +5,21 @@ app.electedOfficial.adapter = {
       url: 'http://api.nytimes.com/svc/politics/v3/us/legislative/congress/113/' + chamber + '/members/current.json?api-key=' + key
     }).then(function(data) {
       var members = data.results[0].members;
-
       members.forEach(function(member) {
-          var electedOfficial = new app.electedOfficial.new(member.first_name, member.last_name, member.party, member.twitter_account, member.state, member.id, member.url);
+        var electedOfficial = new app.electedOfficial.new(member.first_name, member.last_name, member.party, member.twitter_account, member.state, member.id, member.url);
 
-          var official = new app[chamber + "Member"].new(member.seniority, member.next_election, member.total_votes, member.missed_votes, member.missed_votes_pct, member.votes_with_party_pct, electedOfficial);
+        var official = new app[chamber + "Member"].new(member.seniority, member.next_election, member.total_votes, member.missed_votes, member.missed_votes_pct, member.votes_with_party_pct, electedOfficial);
+        // debugger;
+        if(chamber === "house"){
+          official.district = member.district
+        }
       })
       // Returns an Array of Elected Officials who are in a particular state
       // Currently working for Senate Members but not House Members
       var results = _.filter(app[chamber + "Member"].all, function(official) {
         return official.electedOfficial.state === state;
       })
+      debugger;
       return results;
     });
   }
--- app/models/houseMember.js
+++ app/models/houseMember.js
@@ -2,24 +2,24 @@ app.houseMember = {
   all:[],
   new: (function(){
     var counter = 0;
-    var houseMember = function HouseMember(district, houseSeniority, nextElection, totalVotes, missedVotes, missedVotePct,votesWithPartyPct, electedOfficial){
-        this.district = district;
-        this.houseSeniority = houseSeniority;
-        this.nextElection = nextElection;
-        this.totalVotes = totalVotes;
-        this.missedVotes = missedVotes;
-        this.missedVotePct = missedVotePct;
-        this.votesWithPartyPct = votesWithPartyPct;
-        this.electedOfficial = electedOfficial;
+    var houseMember = function HouseMember( houseSeniority, nextElection, totalVotes, missedVotes, missedVotePct,votesWithPartyPct, electedOfficial, district){
+      this.district = district;
+      this.houseSeniority = houseSeniority;
+      this.nextElection = nextElection;
+      this.totalVotes = totalVotes;
+      this.missedVotes = missedVotes;
+      this.missedVotePct = missedVotePct;
+      this.votesWithPartyPct = votesWithPartyPct;
+      this.electedOfficial = electedOfficial;
 
-        var self = this;
-        function initialize(){
-          counter++;
-          self.id = counter;
-          app.houseMember.all.push(self);
-        }
+      var self = this;
+      function initialize(){
+        counter++;
+        self.id = counter;
+        app.houseMember.all.push(self);
+      }
+      initialize();
     }
-
     return houseMember;
 
   }())
