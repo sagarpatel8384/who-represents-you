986b54641a76bfd8a12498b76056cad66ec69f68
sagarpatel8384 <sagarpatel8384@gmail.com>
2016-04-04 16:37:19 -0400

    Update Views

--- app/controllers/senateMembersController.js
+++ app/controllers/senateMembersController.js
@@ -14,28 +14,63 @@ app.senateMember.controller = {
 
         var memberImg = "<img class='member-profile-picture' src = 'https://www.govtrack.us/data/photos/" + data.govtrack_id + ".jpeg'>"
         var memberHTML = `
-        <div>
-          <p><span class="member-label-name">` + data.first_name + " " + data.last_name + `</p>
-          <hr>
-          <p class="member-info-item"><span class="member-label">State: </span>` + member.electedOfficial.state + `</p>
-          <p class="member-info-item"><span class="member-label">Political Party: </span>` + data.current_party + `</p>
-          <p class="member-info-item"><span class="member-label">Date of Birth: </span>` + data.date_of_birth + `</p>
-          <p class="member-info-item"><span class="member-label">Homepage: </span><a href="` + data.url + `" target=_blank>` + data.url + `</a></p>
-          <p class="member-info-item"><span class="member-label">Gender: </span>` + data.gender + `</p>
-        </div>`
+        <p><span class="member-label-name">` + data.first_name + " " + data.last_name + `</p>
+        <table class="table member-table">
+          <tbody>
+            <tr>
+              <th scope="row">State</td>
+              <td>` + member.electedOfficial.state + `</td>
+            </tr>
 
-        var memberInfo = `
-        <div>
-          <p class="member-info-item"><span class="member-label">Seniority: </span>` + member.senateSeniority + `</p>
-          <p class="member-info-item"><span class="member-label">Total Votes in Senate: </span>` + member.totalVotes + `</p>
-          <p class="member-info-item"><span class="member-label">Missed Votes: </span>` + member.missedVotes + `</p>
-          <p class="member-info-item"><span class="member-label">% of Votes Missed in Senate: </span>` + member.missedVotePct + `</p>
-          <p class="member-info-item"><span class="member-label">% of Votes With Political Party: </span>` + member.votesWithPartyPct + `</p>
-        </div>`
+            <tr>
+              <th scope="row">Political Party</td>
+              <td>` + data.current_party + `</td>
+            </tr>
+
+            <tr>
+              <th scope="row">Date of Birth</td>
+              <td>` + data.date_of_birth + `</td>
+            </tr>
+
+            <tr>
+              <th scope="row">Homepage</td>
+              <td><a href="` + data.url + `" target="_blank">` + data.url + `</a></td>
+            </tr>
+
+            <tr>
+              <th scope="row">Gender</td>
+              <td>` + data.gender + `</td>
+            </tr>
+
+            <tr>
+              <th scope="row">Seniority</td>
+              <td>` + member.senateSeniority + `</td>
+            </tr>
+
+            <tr>
+              <th scope="row">Total Votes</td>
+              <td>` + member.totalVotes + `</td>
+            </tr>
+
+            <tr>
+              <th scope="row">Missed Votes:</td>
+              <td>` + member.missedVotes + `</td>
+            </tr>
+
+            <tr>
+              <th scope="row">% of Votes Missed in Senate</td>
+              <td>` + member.missedVotePct + `</td>
+            </tr>
+
+            <tr>
+              <th scope="row">% of Votes With Political Party</td>
+              <td>` + member.votesWithPartyPct + `</td>
+            </tr>
+          <tbody>
+        </table>`
 
         $('.modal-window').append(memberImg)
         $('.modal-window').append(memberHTML)
-        $('.modal-window').append(memberInfo)
       })
     }
   }
